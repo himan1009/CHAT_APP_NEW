44a806592a12820da6fa3a4ee7c4f008e4e1cbcc
himan1009 <shishusha922@gmail.com>
2024-04-13 18:22:29 +0530

    backend robust

---
+++ backend/config/database.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    await mongoose
+        .connect(process.env.MONGO_URI)
+        .then(() => {
+            console.log("DATABASE CONNECTED");
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+};
+
+export default connectDB
---
+++ backend/index.js
@@ -0,0 +1,27 @@
+import express from "express"
+import dotenv from "dotenv"
+import connectDB from "./config/database.js"
+import userRoute from "./routes/userRoute.js";
+import messageRoute from "./routes/messageRoute.js";
+import cookieParser from "cookie-parser";
+//import cors from "cors";
+//import { app,server } from "./socket/socket.js";
+dotenv.config({})
+
+
+const app=express()
+const PORT=process.env.PORT || 8080
+
+// middleware
+app.use(express.json()); 
+app.use(cookieParser())
+
+// routes
+app.use("/api/v1/user",userRoute); 
+app.use("/api/v1/message",messageRoute);
+
+
+app.listen(PORT,()=>{
+    connectDB();
+    console.log(`Server listen at port ${PORT}`)
+})
