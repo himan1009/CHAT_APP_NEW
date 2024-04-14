80431ddfc3c3784bb43dd7721eb20790c2748eaf
himan1009 <shishusha922@gmail.com>
2024-04-14 11:38:34 +0530

    full fledge

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,6 +1,6 @@
 import { Conversation } from "../models/conversationModel.js";
 import { Message } from "../models/messageModel.js";
-
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
 export const sendMessage = async (req,res) => {
     try {
@@ -30,10 +30,10 @@ export const sendMessage = async (req,res) => {
         await Promise.all([gotConversation.save(), newMessage.save()]);
          
         // SOCKET IO
-        // const receiverSocketId = getReceiverSocketId(receiverId);
-        // if(receiverSocketId){
-        //     io.to(receiverSocketId).emit("newMessage", newMessage);
-        // }
+        const receiverSocketId = getReceiverSocketId(receiverId);
+        if(receiverSocketId){
+            io.to(receiverSocketId).emit("newMessage", newMessage);
+        }
         return res.status(201).json({
             newMessage
         })
@@ -41,7 +41,6 @@ export const sendMessage = async (req,res) => {
         console.log(error);
     }
 }
-
 export const getMessage = async (req,res) => {
     try {
         const receiverId = req.params.id;
